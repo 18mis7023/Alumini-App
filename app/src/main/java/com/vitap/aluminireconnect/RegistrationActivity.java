71c0531b1db6d3444ab0e7c25025793984c9aae9
sai123gopal <sai.gopalsai143@gmail.com>
2021-07-31 16:20:43 +0530

    created RegistrationActivity

---
+++ app/src/main/java/com/vitap/aluminireconnect/RegistrationActivity.java
@@ -0,0 +1,17 @@
+package com.vitap.aluminireconnect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+
+
+    }
+}
